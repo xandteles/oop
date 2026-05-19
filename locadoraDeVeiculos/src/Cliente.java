b86d5a13f16e684b204648af4e51304df33e0501
Alexandre Teles <xandeneto1803@gmail.com>
2026-05-19 12:17:15 -0300

    Add files via upload

---
+++ locadoraDeVeiculos/src/Cliente.java
@@ -0,0 +1,37 @@
+public class Cliente {
+
+private String cpf;
+private String nome;
+private int idade;
+
+public Cliente(String cpf, String nome, int idade) {
+this.cpf = cpf;
+this.nome = nome;
+this.idade = idade;
+
+}
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
---
+++ locadoraDeVeiculos/src/Locacao.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Locacao {
+
+private double valorTotal;
+private LocalDate dataInicio;
+private LocalDate dataFim;
+
+public Locacao(double valorTotal, LocalDate dataInicio, LocalDate dataFim) {
+this.valorTotal = valorTotal;
+this.dataInicio = dataInicio;
+this.dataFim = dataFim;
+
+}
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+    long diasDeDiferenca = ChronoUnit.DAYS.between(dataInicio, dataFim);
+
+//falta o metodo;
+//teste git push
+
+}
