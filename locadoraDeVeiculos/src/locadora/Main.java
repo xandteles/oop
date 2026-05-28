5b6673df25a9b97c0c00bfde40c5e55d55a811b4
XandTeles <xandeneto1803@gmail.com>
2026-05-28 19:51:29 -0300

    Add files via upload

--- locadoraDeVeiculos/src/locadora/Main.java
+++ locadoraDeVeiculos/src/locadora/Main.java
@@ -1,5 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package locadora;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
---
+++ locadoraDeVeiculos/src/locadora/Veiculo.java
@@ -0,0 +1,33 @@
+package locadora;
+
+public abstract class Veiculo {
+
+protected String placa, modelo;
+protected double valorDiaria;
+
+public Veiculo(String placa, String modelo, double valorDiaria){
+
+this.placa = placa;
+this.modelo = modelo;
+this.valorDiaria = valorDiaria;
+}
+
+    public double getValorDiaria() {
+        return valorDiaria;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getplaca() {
+
+return placa;
+}
+
+public double calcularValorLocacao() {
+
+return 0.0;
+}
+
+}
