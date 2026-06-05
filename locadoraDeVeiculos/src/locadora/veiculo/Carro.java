136a7b3d573a353449dade0bbcb3602b3d6084ee
Dario <143418773+dariomartfeld@users.noreply.github.com>
2026-06-05 12:10:10 -0300

    Update Carro.java

--- locadoraDeVeiculos/src/locadora/veiculo/Carro.java
+++ locadoraDeVeiculos/src/locadora/veiculo/Carro.java
@@ -1,16 +1,14 @@
 package locadora.veiculo;
 
-public class Carro extends Veiculo {
-
-    public Carro(String placa, String modelo, double valorDiaria) {
-
+public class Carro extends Veiculo{
+    public Carro(String placa, String modelo, double valorDiaria){
         super(placa, modelo, valorDiaria);
 
     }
 
     @Override
-    public double calcularValorLocacao(long dias) {
-        return 0;
+    public double calcularValorLocacao(int dias){
+        return getValorDiaria() * dias;
     }
-
 }
+
